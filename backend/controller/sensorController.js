878af13cd70ad0cd80a55abc89db13f8528573ab
Nice <hoangkimc93@gmail.com>
2023-03-22 01:23:43 +0700

    add promise for find() callback

--- backend/controller/sensorController.js
+++ backend/controller/sensorController.js
@@ -2,12 +2,8 @@ import { SensorModel } from "../model/sensor_model.js";
 
 export default new (class SensorController {
     testing = async (req, res) => {
-        SensorModel.find({}, (err, users) => {
-            if (err) {
-                res.status(500).send('Error getting users');
-            } else {
-                res.json(users);
-            }
-        });
+        await SensorModel.find({})
+            .then(data => res.json(data))
+            .catch(err => res.status(500).send('Error getting users'))
     }
 })();
